ea86e042f8fc3b3960b5b898a792ea21aec6a330
zpollar0 <zpollard2000@gmail.com>
2024-05-24 11:47:46 -0400

    First push. Completed Shape and Starting Circle classes

---
+++ src/main/java/com/pluralsight/Shape.java
@@ -0,0 +1,45 @@
+package com.pluralsight;
+import java.awt.Color;
+import java.awt.Point;
+
+public abstract class Shape {
+    private World world;
+    Color color;
+    double border;
+
+    //Create constructor
+    public Shape(World world){
+        this.world = world;
+    }
+
+    //Create getters and setters
+    public World getWorld() {
+        return world;
+    }
+
+    public void setWorld(World world) {
+        this.world = world;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public double getBorder() {
+        return border;
+    }
+
+    public void setBorder(double border) {
+        this.border = border;
+    }
+    //Make abstract methods
+    public abstract void setLocation(Point location);
+
+    public abstract void paint();
+
+    public abstract void getLocation();
+}
